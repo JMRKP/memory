c093cba9eb299eb68ecf0657b8d997a486eac2ff
jp063 <jp063@hdm-stuttgart.de>
2018-02-09 12:27:18 +0100

    * create eventsender.clj * move multicast-game-to-all-its-players to eventsender

--- src/memory/server/eventhandler.clj
+++ src/memory/server/eventhandler.clj
@@ -5,8 +5,3 @@
 
 (defn create-game [uid]
     (games/add-new-game uid))
-
-(defn mulitcast-current-game-to-all-its-players [game-id]
-  (let [{players :players :as game} (:game-id @games/games)]
-        (doseq [uid (vals players)]
-            (websocket/chsk-send! uid [:game/play game]))
